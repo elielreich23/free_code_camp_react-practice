f927007448badeb892749d2d41728e6ab131ff49
elielreich23 <elielreich@yahoo.com>
2022-12-30 20:03:15 -0300

    pratice

--- card/src/App.js
+++ card/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import BlockImg from "./images/Rectangle 90.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <img src={BlockImg} alt="" />
+        <div className="content">
+          <h1>Laura Smith</h1>
+          <h5>Frontend Developper</h5>
+          <h6>laurasmith.website</h6>
+          <div className="btns">
+            {/* btn class will have styling to split <vbutton into two on hover see figma */}
+            <button type="submit" className="email">Email</button>
+            <button className="linkdin">
+              Linkedin
+            </button>
+          </div>
+          <div className="details-sec">
+            <h3 className="about-title">About</h3>
+            <p className="about">
+              I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+            </p>
+            <h3 className="interest-title">Interest</h3>
+            <p className="interest">
+              Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+            </p>
+          </div>
+        </div>
+        <div className="footer">
+          <div className="social-media">
+
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
